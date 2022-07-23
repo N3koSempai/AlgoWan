38fa7524f6144337e94a9ea7a4251efcc2f9c0c3
N3koSempai <alvarom12098@gmail.com>
2022-07-23 16:08:36 -0400

    basic template structure for login in the app
    = this is only a template for the structure
    - All the Graphical structure is in algowangui.kv
    - the logic for the login is in AlgoWan.py for now

--- AlgoWan.py
+++ AlgoWan.py
@@ -1,4 +1,47 @@
- 
-import kivy
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+import time
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+Window.size=(300,450)
 
-from kivy.uix.gridlayout import GridLayout
+
+
+class LoginApp(MDApp):
+    dialog = None
+
+    def build(self):
+
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.primary_palet = "Red"
+        self.theme_cls.accent_palette = "Green"
+
+        return Builder.load_file("Algowangui.kv")
+
+
+
+
+    def login(self):
+        if self.root.ids.user.text == 'admin' and self.root.ids.password.text == '123':
+            if not self.dialog:
+                self.dialog = MDDialog(
+
+                    title = 'Login',
+                    text = f"Bienvenido {self.root.ids.user.text}!",
+                    buttons = [
+                        MDFlatButton(
+                        text = "Ok", text_color = self.theme_cls.accent_color
+
+                        ),],
+
+                )
+                self.dialog.open()
+
+    def close(self):
+        self.dialog.dismiss()
+
+
+if __name__ == "__main__":
+    LoginApp().run()
